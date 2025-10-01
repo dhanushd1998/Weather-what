375b78c88b600f68708b0efb6a3780b8eaa87d61
dhanushd1998 <dhanushdevadiga1998@gmail.com>
2025-10-01 16:50:42 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -192,7 +192,7 @@ function App() {
 
             <UTCDatetime />
             <Link
-              href="https://github.com/Amin-Awinti"
+              href="https://github.com/dhanushd1998/"
               target="_blank"
               underline="none"
               sx={{ display: 'flex' }}
